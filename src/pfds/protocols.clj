1cefccbbe92a1356edf3c6c0a7aa8ea2c72611a1
Roland Clairquin <rolandcl@azrael.fontaine>
2017-09-11 21:59:48 +0200

    Refactorisation, creation d'un fichier séparé pour les protocols, OK pour leftist_heap

---
+++ src/pfds/protocols.clj
@@ -0,0 +1,11 @@
+(ns pfds.protocols)
+
+(defprotocol HEAP
+  (heap-empty?     [heap])
+  (heap-merge      [h1 h2])
+  (heap-insert     [heap x])
+  (heap-find-min   [heap])
+  (heap-delete-min [heap]))
+
+
+
